e451866c8da99d77469a8fc741381255100e2afb
blackfenzer <6431346921@student.chula.ac.th>
2025-03-15 19:00:02 +0700

    auth crud model

--- app/routes/crud_model.py
+++ app/routes/crud_model.py
@@ -1,26 +1,14 @@
 # Add these to your FastAPI router
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
-from datetime import datetime
-from app.schemas.schemas import AllModelResponse, AllModelUpdate
+from app.schemas.schemas import AllModelResponse, AllModelUpdate, UserSchema
 from app.database.session import get_db
 from app.models.schema import Model
 import bentoml
 
+from app.routes.auth2 import protected_route
+
 router = APIRouter()
-
-
-# @router.post("/models/", response_model=ModelResponse)
-# def create_model(model: ModelCreate, db: Session = Depends(get_db)):
-#     db_model = Model(**model.dict(), created_at=datetime.now())
-#     db.add(db_model)
-#     try:
-#         db.commit()
-#         db.refresh(db_model)
-#     except Exception as e:
-#         db.rollback()
-#         raise HTTPException(status_code=400, detail=str(e))
-#     return db_model
 
 
 @router.get("/", response_model=list[AllModelResponse])
@@ -43,7 +31,12 @@ def read_model(model_name: str, db: Session = Depends(get_db)):
 
 
 @router.put("/models/{model_name}", response_model=AllModelResponse)
-def update_model(model_name: str, model: AllModelUpdate, db: Session = Depends(get_db)):
+def update_model(
+    model_name: str,
+    model: AllModelUpdate,
+    db: Session = Depends(get_db),
+    user: UserSchema = Depends(protected_route),
+):
     db_model = db.query(Model).filter(Model.name == model_name).first()
     if not db_model:
         raise HTTPException(status_code=404, detail="Model not found")
@@ -62,7 +55,11 @@ def update_model(model_name: str, model: AllModelUpdate, db: Session = Depends(g
 
 
 @router.delete("/{model_name}")
-def delete_model(model_name: str, db: Session = Depends(get_db)):
+def delete_model(
+    model_name: str,
+    db: Session = Depends(get_db),
+    user: UserSchema = Depends(protected_route),
+):
     db_model = (
         db.query(Model)
         .filter(Model.name == model_name, Model.is_active == True)
